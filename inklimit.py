bbd0d2c7c48301b7014522328fa9472d75927384
Di Wu <di.wu.dw@gmail.com>
2022-08-16 10:56:18 -0700

    Implemented linear scaling of image

--- inklimit.py
+++ inklimit.py
@@ -1,17 +1,30 @@
-def inklimit(inklimit = 400):
-    from PIL import Image
-    import numpy as np
+from PIL import Image
+import numpy as np
+import sys
 
-    # path ends with ".tiff"
-    path = '/Users/di/Dropbox/Code/GitHub/pairprogramming/testimage.tiff'
-    img = Image.open(path, 'r')
+def inklimit():
+    
+    # read in system arguments 
+    inklimit = int(sys.argv[3])
+    out_name = sys.argv[2]
+    in_name = sys.argv[1]
+
+    # open in_name file 
+    img = Image.open(in_name, 'r')
     arr = np.asarray(img)
 
-    print('arr.shape')
+    # inklimiting scale factor
+    scale_img = inklimit/400
+    arr_copy = np.copy(arr)
+    scaled_arr = np.multiply(scale_img, arr_copy)
 
-    chlimit = inklimit/4
-    # map 0-255 onto 0-chlimit using numpy operation
-    print(arr[0,0,0])
+    # cast array as uint8 
+    scaled_arr_cast = scaled_arr.astype('uint8')
+
+    # save images to out_name path
+    scaled_img = Image.fromarray(scaled_arr_cast, mode = 'CMYK')
+    scaled_img.save(out_name, compression = 'tiff_deflate')
+
     return
 
 inklimit()
